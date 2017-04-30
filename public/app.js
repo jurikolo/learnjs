ee3be97309c963066e289d1bfcc79404e30c17ca
Jurijs Kolomijecs <jurikolo@yandex.com>
2017-04-30 21:22:59 +0300

    Small bugfixes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,13 @@
-'use strict';
+/***
+ * Excerpted from "Serverless Single Page Apps",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/brapps for more book information.
+ ***/
+"use strict";
+
 var learnjs = {};
 
 learnjs.problems = [
@@ -8,13 +17,46 @@ learnjs.problems = [
     },
     {
         description: "Simple Math",
-        code: "function problem() { return 42 === 6 * __"
+        code: "function problem() { return 42 === 6 * __; }"
     }
 ];
 
-learnjs.problemView = function (data) {
+learnjs.triggerEvent = function(name, args) {
+    $('.view-container>*').trigger(name, args);
+};
+
+learnjs.template = function(name) {
+    return $('.templates .' + name).clone();
+};
+
+learnjs.applyObject = function(obj, elem) {
+    for (var key in obj) {
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
+
+learnjs.flashElement = function(elem, content) {
+    elem.fadeOut('fast', function() {
+        elem.html(content);
+        elem.fadeIn();
+    });
+};
+
+learnjs.buildCorrectFlash = function (problemNum) {
+    var correctFlash = learnjs.template('correct-flash');
+    var link = correctFlash.find('a');
+    if (problemNum < learnjs.problems.length) {
+        link.attr('href', '#problem-' + (problemNum + 1));
+    } else {
+        link.attr('href', '');
+        link.text("You're Finished!");
+    }
+    return correctFlash;
+};
+
+learnjs.problemView = function(data) {
     var problemNumber = parseInt(data, 10);
-    var view = $('.templates .problem-view').clone();
+    var view = learnjs.template('problem-view');
     var problemData = learnjs.problems[problemNumber - 1];
     var resultFlash = view.find('.result');
 
@@ -34,60 +76,42 @@ learnjs.problemView = function (data) {
         return false;
     }
 
+    if (problemNumber < learnjs.problems.length) {
+        var buttonItem = learnjs.template('skip-btn');
+        buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+        $('.nav-list').append(buttonItem);
+        view.bind('removingView', function() {
+            buttonItem.remove();
+        });
+    }
+
     view.find('.check-btn').click(checkAnswerClick);
     view.find('.title').text('Problem #' + problemNumber);
     learnjs.applyObject(problemData, view);
     return view;
 };
 
-learnjs.showView = function (hash) {
+learnjs.landingView = function() {
+    return learnjs.template('landing-view');
+};
+
+learnjs.showView = function(hash) {
     var routes = {
         '#problem': learnjs.problemView,
-        '': learnjs.landingView()
+        '#': learnjs.landingView,
+        '': learnjs.landingView
     };
     var hashParts = hash.split('-');
     var viewFn = routes[hashParts[0]];
     if (viewFn) {
+        learnjs.triggerEvent('removingView', []);
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 };
 
-learnjs.appOnReady = function () {
-    window.onhashchange = function () {
+learnjs.appOnReady = function() {
+    window.onhashchange = function() {
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
-};
-
-learnjs.applyObject = function (obj, elem) {
-    for (var key in obj) {
-        elem.find('[data-name="' + key + '"]').text(obj[key]);
-    }
-};
-
-learnjs.flashElement = function (elem, content) {
-    elem.fadeOut('fast', function () {
-        elem.html(content);
-        elem.fadeIn();
-    });
-};
-
-learnjs.template = function (name) {
-    return $('.templates .' + name).clone();
-};
-
-learnjs.buildCorrectFlash = function (problemNum) {
-    var correctFlash = learnjs.template('correct-flash');
-    var link = correctFlash.find('a');
-    if (problemNum < learnjs.problems.length) {
-        link.attr('href', '#problem-' + (problemNum + 1));
-    } else {
-        link.attr('href', '');
-        link.text("You're Finished!");
-    }
-    return correctFlash;
-};
-
-learnjs.landingView = function () {
-    return learnjs.template('landing-view');
 };
